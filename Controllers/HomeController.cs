85d7aa6ba39805e2735800242207c7fdfa4a800c
Amahle Nyangani <amahle.nyangani@dutappfactory.co.za>
2022-03-27 21:16:25 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using HandyMan.Business;
+using HandyMan.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace HandyMan.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public ActionResult GetUsers()
+        {
+            var business = new UsersBusiness();
+            var user = business.GetUsers();
+            return Json(JsonSerializer.Serialize(user));
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ StartUp.cs
@@ -0,0 +1,90 @@
+﻿using HandyMan.Business.Configuration;
+using HandyMan.Data;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+
+namespace HandyMan
+{
+    public class StartUp
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.HandyManConnectionStringService(AppSettings.GetHandyManConnectionString());
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+
+            services.AddDbContext<IdentityContext>(options =>
+            options.UseSqlServer(AppSettings.GetHandyManConnectionString()));
+
+            services.Configure<IISServerOptions>(options =>
+            {
+                options.AllowSynchronousIO = true;
+            });
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            // Add other security headers
+
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseStatusCodePages(async context =>
+            {
+                if (context.HttpContext.Request.Path.StartsWithSegments("/api"))
+                {
+                    if (!context.HttpContext.Response.ContentLength.HasValue || context.HttpContext.Response.ContentLength == 0)
+                    {
+                        // You can change ContentType as json serialize
+                        context.HttpContext.Response.ContentType = "text/plain";
+                        await context.HttpContext.Response.WriteAsync($"Status Code: {context.HttpContext.Response.StatusCode} - {ReasonPhrases.GetReasonPhrase(context.HttpContext.Response.StatusCode)}");
+                    }
+                }
+                else
+                {
+                    // You can ignore redirect
+                    context.HttpContext.Response.Redirect($"/Error/{context.HttpContext.Response.StatusCode}");
+                }
+            });
+
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                SupportedCultures = new List<CultureInfo> { new CultureInfo("en-GB") },
+                SupportedUICultures = new List<CultureInfo> { new CultureInfo("en-GB") },
+                DefaultRequestCulture = new RequestCulture("en-GB")
+            };
+            app.UseRequestLocalization(localizationOptions);
+
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseRouting();
+            app.UseCors();
+            app.UseResponseCaching();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+
+        }
+    }
+}
