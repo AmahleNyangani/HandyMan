caa9bc3559b0ccd9cc9fe1fd59e492cfeed127ae
Amahle Nyangani <amahlenyangani@gmail.com>
2024-08-30 13:14:27 +0200

    New changes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using System.Text.Json;
 using HandyMan.ViewModel;
+using PagedList;
 
 namespace HandyMan.Controllers
 {
@@ -26,6 +27,11 @@ namespace HandyMan.Controllers
           
             return View();
         }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult AddUser(UsersViewModel model)
@@ -34,11 +40,17 @@ namespace HandyMan.Controllers
             business.AddUser(model);
             return View();
         }
-            public ActionResult GetUsers(string type)
+        public ActionResult GetUsers(string type, int page = 1)
         {
+            int pageSize = 10; // Number of items per page
             var business = new UsersBusiness();
-            var users = business.GetUsers(type);
-            return View(users);
+            var users = business.GetUsers(type); // Retrieve the list of users based on the type
+
+            // Convert the list of users to a paged list
+            var pagedList = users.ToPagedList(page, pageSize);
+
+            // Pass the paged list to the view
+            return View(pagedList);
         }
         //public ActionResult GetUserById(int UserId)
         //{
@@ -68,6 +80,7 @@ namespace HandyMan.Controllers
             new UsersBusiness().UpdateUser(model);
             return View();
         }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
